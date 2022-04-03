6c3386fa8e13432831963957920ecedcde9dd34d
Ali Can Tozlu <alicantozlu@gmail.com>
2022-04-03 16:38:48 +0300

    SwiftHelpers And CollectionViewRegister Packages created And Added To Project

--- UserCommentsApp/UserCommentsApp/SplashScreen.swift
+++ UserCommentsApp/UserCommentsApp/SplashScreen.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
+import SwiftHelperFunctions
 
 class SplashScreen: UIViewController {
 
+    let helper = SwiftHelper()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         DispatchQueue.main.asyncAfter(deadline: .now()+1){
-            screenPresenter(selfName: self, targetIdentifier: "userScreenIdentifier", presentation: .fullScreen, transition: .flipHorizontal, completionText:("SplashScreen'e Geçildi"))
+            self.helper.screenPresenter(selfName: self, targetIdentifier: "userScreenIdentifier", presentation: .fullScreen, transition: .flipHorizontal, completionText:("SplashScreen'e Geçildi"))
         }
     }
 }
